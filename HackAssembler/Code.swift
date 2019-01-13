d89a528b63da31a9fb8e163edb942a2eee7ed347
syatyo <yoshiharuseijin@gmail.com>
2019-01-13 15:51:49 +0900

    feat: implement comp in code.

--- HackAssembler/Code.swift
+++ HackAssembler/Code.swift
@@ -17,6 +17,45 @@ struct Code {
         return "\(d1)\(d2)\(d3)"
     }
     
+    // TODO: Is there a any way to implement more efficiently? I don't want to use all pattern matching.
+    static func comp(from mnemonic: String) -> String {
+        
+        // I did not use new line to notice shortage of bit easily.
+        switch mnemonic {
+        // a bit is 0.
+        case "0":   return "0101010"
+        case "1":   return "0111111"
+        case "-1":  return "0111010"
+        case "D":   return "0001100"
+        case "A":   return "0110000"
+        case "!D":  return "0001101"
+        case "!A":  return "0110001"
+        case "-D":  return "0001111"
+        case "-A":  return "0110011"
+        case "D+1": return "0011111"
+        case "A+1": return "0110111"
+        case "D-1": return "0001110"
+        case "A-1": return "0110010"
+        case "D+A": return "0000010"
+        case "D-A": return "0010011"
+        case "A-D": return "0000111"
+        case "D&A": return "0000000"
+        case "D|A": return "0010101"
+            
+        // a bit is 1.
+        case "M":   return "1110000"
+        case "!M":  return "1110001"
+        case "-M":  return "1110011"
+        case "M+1": return "1110111"
+        case "M-1": return "1110010"
+        case "D+M": return "1000010"
+        case "D-M": return "1010011"
+        case "M-D": return "1000111"
+        case "D&M": return "1000000"
+        case "D|M": return "1010101"
+        default: fatalError() // Implementation failure.
+        }
+    }
 }
 
 extension Bool {
--- HackAssemblerTests/CodeTests.swift
+++ HackAssemblerTests/CodeTests.swift
@@ -27,4 +27,41 @@ class CodeTests: XCTestCase {
             XCTAssertEqual(binary, expectations[index])
         }
     }
+    
+    func testComp() {
+        let testMnemonics: [(String, String)] = [("0",   "0101010"),
+                                                 ("1",   "0111111"),
+                                                 ("-1",  "0111010"),
+                                                 ("D",   "0001100"),
+                                                 ("A",   "0110000"),
+                                                 ("!D",  "0001101"),
+                                                 ("!A",  "0110001"),
+                                                 ("-D",  "0001111"),
+                                                 ("-A",  "0110011"),
+                                                 ("D+1", "0011111"),
+                                                 ("A+1", "0110111"),
+                                                 ("D-1", "0001110"),
+                                                 ("A-1", "0110010"),
+                                                 ("D+A", "0000010"),
+                                                 ("D-A", "0010011"),
+                                                 ("A-D", "0000111"),
+                                                 ("D&A", "0000000"),
+                                                 ("D|A", "0010101"),
+                                                 ("M",   "1110000"),
+                                                 ("!M",  "1110001"),
+                                                 ("-M",  "1110011"),
+                                                 ("M+1", "1110111"),
+                                                 ("M-1", "1110010"),
+                                                 ("D+M", "1000010"),
+                                                 ("D-M", "1010011"),
+                                                 ("M-D", "1000111"),
+                                                 ("D&M", "1000000"),
+                                                 ("D|M", "1010101")]
+        
+        for (index, result) in testMnemonics.enumerated() {
+            let binary = Code.comp(from: result.0)
+            XCTAssertEqual(binary, result.1, "index is \(index)")
+        }
+    }
+    
 }
